53b021b9cb50ff89013800cd3fff611de3ad554e
Nathan Rapport <nathan.rapport@cfa.harvard.edu>
2018-12-12 12:24:25 -0500

    adding database and table

--- config.py
+++ config.py
@@ -7,6 +7,14 @@ class ProdConfig:
 	pass
 
 class DevConfig:
+	DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+		user=os.environ['POSTGRES_USER'],
+		pw=os.environ['POSTGRES_PW'],
+		url=os.environ['POSTGRES_URL'],
+		db=os.environ['POSTGRES_DB'])
+
 	DEBUG = True
-	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+	SQLALCHEMY_DATABASE_URI = DB_URL
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
--- main.py
+++ main.py
@@ -10,5 +10,21 @@ db = SQLAlchemy(app)
 def home():
 	return '<h1>Hello cruel world</h1>'
 
+class Page(db.Model):
+	qid = db.Column(db.Integer(), primary_key=True)
+	content_type = db.Column(db.String(255))
+	content = db.Column(db.Text())
+	created = db.Column(db.DateTime())
+	updated = db.Column(db.DateTime())
+	expires = db.Column(db.DateTime())
+	lifetime = db.Column(db.Time())
+	owner = db.Column(db.Integer())
+
+	def __init__(self, content):
+		self.content = content
+
+	def __repr__(self):
+		return "<qid {}> {}".format(self.qid, self.content)
+
 if __name__=='__main__':
 	app.run()
